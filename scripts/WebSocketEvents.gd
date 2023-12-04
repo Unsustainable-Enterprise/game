ad3926e6ddafd0b7989d77d87a5a64cffbb194a8
CaptainBulba <varavaivan1@gmail.com>
2023-12-04 19:43:51 +0200

    added new events to websocketevents

--- scripts/WebSocketEvents.gd
+++ scripts/WebSocketEvents.gd
@@ -1,5 +1,5 @@
 extends Node
 
-const CREATE_LOBBY = "create_lobby"
+const CREATE_LOBBY = "create_lobby" 
 const JOIN_LOBBY = "join_lobby"
 const PARTICIPANT_JOINED_LOBBY = "participant_joined_lobby"
