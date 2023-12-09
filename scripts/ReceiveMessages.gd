274571e75435b03b9addd5d0652f0a54b97745f7
CaptainBulba <varavaivan1@gmail.com>
2023-12-09 18:47:03 +0200

    added types to ReceiveMessages

--- scripts/ReceiveMessages.gd
+++ scripts/ReceiveMessages.gd
@@ -2,28 +2,27 @@ extends Node
 
 onready var data_manager: Node = get_node("/root/DataManager")
 
-func create_lobby(data):
-	print("create_lobby_response")
-	print(data)
+func create_lobby(data: Dictionary):
 	add_data_manager(data.token, data.message.user_name, [data.message.user_name], true, data.message.pin)
+
 	if get_tree().change_scene("res://scenes/Lobby.tscn") != OK:
 		print ("An unexpected error occured when trying to switch scenes")
 
-func join_lobby(data):
-		print("join_lobby_response")
-		print(data)
+func join_lobby(data: Dictionary):
 		add_data_manager(data.token, data.message.user_name, data.message.participants, false)
+		
 		if get_tree().change_scene("res://scenes/Lobby.tscn") != OK:
 			print ("An unexpected error occured when trying to switch scenes")
 
-func participant_joined_lobby(data):
+func participant_joined_lobby(data: Dictionary):
 	print("participant_joined_lobby")
 	print(data)
 
-func add_data_manager(token, user_name, participants, is_host, pin = ""):
+func add_data_manager(token: String, user_name: String, participants: Array, is_host: bool, pin: String = ""):
+	if(is_host):
+		data_manager.set_pin(pin)
+
 	data_manager.set_token(token)
 	data_manager.set_user_name(user_name)
 	data_manager.set_participants(participants)
 	data_manager.set_is_host(is_host)
-	if(is_host):
-		data_manager.set_pin(pin)
